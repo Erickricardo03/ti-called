4d33d690733177f84fbadc5ea247d18999cee7de
Erick Ricardo <edck030303@gmail.com>
2025-08-26 11:21:45 -0300

    Primeiro commit

---
+++ src/main/java/Model/User.java
@@ -0,0 +1,18 @@
+package Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table( name = "user")
+@SequenceGenerator(name = "seq_user")
+public class User  {
+
+	
+
+		
+
+	}
+
+
